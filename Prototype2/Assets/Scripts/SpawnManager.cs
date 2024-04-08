49ecfb60711d63605547c0ef7ca4218d7c76006a
2025diego <142940160+2025diego@users.noreply.github.com>
2024-04-08 12:58:06 -0700

    Removed Unused Library. Serialized Public Property. Made String Const. SpawnManager.cs

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     //animal prefabs
-    public GameObject[] animalPrefabs;
+    [SerializedField] private GameObject[] animalPrefabs;
     //x range
     private float spawnRangeX = 20.0f;
     //z position
@@ -21,7 +20,8 @@ public class SpawnManager : MonoBehaviour
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
-
+    //Change this string to match the name of the following method
+     private const string spawnAnimalsMethod = "SpawnRandomAnimal"
     //Method to spawn animals
     private void SpawnRandomAnimal()
     {
